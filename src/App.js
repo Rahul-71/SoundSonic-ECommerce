6c6a7433fbf87f8a02962ca8bd43406bfeaf7c26
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-27 00:02:17 +0530

    initial structure & router design of app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Header from "./components/Header/Header";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+import Footer from "./components/Footer/Footer";
+
 function App() {
-    return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+      </Routes>
+      <Newsletter />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
