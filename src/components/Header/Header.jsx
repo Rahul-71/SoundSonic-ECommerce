bfff5905e0790046d4dd877ea777f6dd41d1b58d
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-27 23:13:31 +0530

    Header component structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,28 @@
+import { BiSearch, BiHeart, BiCart } from "react-icons/bi";
+
 import "./Header.scss";
+
 const Header = () => {
-    return <div>Header</div>;
+  return (
+    <header className="main-header">
+      <div className="header-content">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Categories</li>
+        </ul>
+        <div className="center">Sound Sonic</div>
+        <div className="right">
+          <BiSearch />
+          <BiHeart />
+          <span className="cart-icon">
+            <BiCart />
+            <span>5</span>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
