1ccd88fc2fa370c1857997ad875783a13ccf69a3
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-30 11:29:40 +0530

    fix: moving categories component under home

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,26 +1,6 @@
-import cat1 from "../../assets/category/cat-1.jpg";
-
 import "./Category.scss";
-
 const Category = () => {
-  return (
-    <div className="shop-by-category">
-      <div className="categories">
-        <div className="category">
-          <img src={cat1} alt="" />
-        </div>
-        <div className="category">
-          <img src={cat1} alt="" />
-        </div>
-        <div className="category">
-          <img src={cat1} alt="" />
-        </div>
-        <div className="category">
-          <img src={cat1} alt="" />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Category</div>;
 };
 
 export default Category;
