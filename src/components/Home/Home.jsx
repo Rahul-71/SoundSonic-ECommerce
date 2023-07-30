f0da1c3979126a5cab790cd49a1c9a2b40e1364d
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-30 10:22:59 +0530

    categories section included

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,16 @@
+import Category from "../Category/Category";
 import Banner from "./Banner/Banner";
 
 import "./Home.scss";
 const Home = () => {
   return (
-    <div className="home">
+    <div>
       <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+        </div>
+      </div>
     </div>
   );
 };
