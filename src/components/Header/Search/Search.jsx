80f64870ef38f8a24229aace3884633cda1e0c67
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-31 00:09:51 +0530

    search result component with styling

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,11 +1,28 @@
 import { MdClose } from "react-icons/md";
+import prodImg from "../../../assets/products/earbuds-prod-1.webp";
+
 import "./Search.scss";
+
 const Search = ({ setShowSearch }) => {
   return (
     <div className="search-modal">
       <div className="form-field">
         <input type="text" autoFocus placeholder="Search for product" />
         <MdClose onClick={() => setShowSearch(false)} />
+      </div>
+
+      <div className="search-result-content">
+        <div className="search-results">
+          <div className="search-result-item">
+            <div className="img-container">
+              <img src={prodImg} alt="" />
+            </div>
+            <div className="prod-details">
+              <span className="name">product name</span>
+              <span className="desc">product desc</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
