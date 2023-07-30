67eb253ee1ec80df965ded6e705d993d56a0e9b8
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-30 17:06:14 +0530

    cart design included under header with modal styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,26 @@
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
 import "./Cart.scss";
-const Cart = () => {
-    return <div>Cart</div>;
+const Cart = ({ setShowCart }) => {
+  return (
+    <div className="cart-panel">
+      <div
+        className="opac-layer"
+        onClick={() => {
+          setShowCart(false);
+        }}
+      ></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shopping Cart</span>
+          <span className="close-btn" onClick={() => setShowCart(false)}>
+            <MdClose />
+            <span className="text">Close</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,11 @@ import { BiSearch, BiHeart, BiCart } from "react-icons/bi";
 
 import "./Header.scss";
 import { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
 
   const handleScrolled = () => {
     const scrollOffset = window.scrollY;
@@ -17,24 +19,27 @@ const Header = () => {
   }, []);
 
   return (
-    <header className={`main-header ${scrolled ? "stickyHeader" : ""}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
-        <div className="center">Sound Sonic</div>
-        <div className="right">
-          <BiSearch />
-          <BiHeart />
-          <span className="cart-icon">
-            <BiCart />
-            <span>5</span>
-          </span>
+    <>
+      <header className={`main-header ${scrolled ? "stickyHeader" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Categories</li>
+          </ul>
+          <div className="center">Sound Sonic</div>
+          <div className="right">
+            <BiSearch />
+            <BiHeart />
+            <span className="cart-icon" onClick={() => setShowCart(!showCart)}>
+              <BiCart />
+              <span>5</span>
+            </span>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {showCart && <Cart setShowCart={setShowCart} />}
+    </>
   );
 };
 
