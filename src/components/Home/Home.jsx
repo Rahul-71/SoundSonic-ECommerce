f2f6953ac050052eb1c39496b571c9db7b101be9
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-30 11:27:19 +0530

    products section included

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Category from "../Category/Category";
+import Products from "../Products/Products";
 import Banner from "./Banner/Banner";
 
 import "./Home.scss";
@@ -9,6 +10,7 @@ const Home = () => {
       <div className="main-content">
         <div className="layout">
           <Category />
+          <Products />
         </div>
       </div>
     </div>
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,17 @@
+import prodImg from "../../../assets/products/earbuds-prod-1.webp";
 import "./Product.scss";
 const Product = () => {
-    return <div>Product</div>;
+  return (
+    <div className="product-card">
+      <div className="thumbnail">
+        <img src={prodImg} alt="" />
+      </div>
+      <div className="prod-details">
+        <span className="name">Product</span>
+        <span className="price"> ₹499</span>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
