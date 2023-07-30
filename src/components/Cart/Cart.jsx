caa06a806ebacdc06f67fc0b475b785b822b9ef1
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-30 23:15:54 +0530

    cart footer added into cart modal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import { MdClose } from "react-icons/md";
-import { BsCartX } from "react-icons/bs";
 import "./Cart.scss";
+import CartItem from "./CartItem/CartItem";
 const Cart = ({ setShowCart }) => {
   return (
     <div className="cart-panel">
@@ -18,6 +18,25 @@ const Cart = ({ setShowCart }) => {
             <span className="text">Close</span>
           </span>
         </div>
+
+        {/* <div className="empty-cart">
+          <BsCartX />
+          <span>No products in the cart.</span>
+          <button className="return-cta">RETURN TO SHOP</button>
+        </div> */}
+
+        <>
+          <CartItem />
+          <div className="cart-footer">
+            <div className="subtotal">
+              <span className="text">Subtotal: </span>
+              <span className="text total"> ₹4999</span>
+            </div>
+            <div className="button">
+              <button className="return-cta">Proceed to Checkout</button>
+            </div>
+          </div>
+        </>
       </div>
     </div>
   );
