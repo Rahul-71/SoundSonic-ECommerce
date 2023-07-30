acb917263b9a6cfee16b00aab567da11370b3fd3
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-07-30 23:17:23 +0530

    styling of items present in cart section

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,31 @@
+import { MdClose } from "react-icons/md";
+import prodImg from "../../../assets/products/earbuds-prod-1.webp";
 import "./CartItem.scss";
 const CartItem = () => {
-    return <div>Cart item</div>;
+  return (
+    <div className="cart-products">
+      <div className="cart-product">
+        <div className="img-container">
+          <img src={prodImg} alt="" />
+        </div>
+        <div className="prod-details">
+          <span className="name">product name</span>
+          <MdClose className="close-btn" />
+          <div className="quantity-buttons">
+            <span>-</span>
+            <span>5</span>
+            <span>+</span>
+          </div>
+
+          <div className="text">
+            <span>3</span>
+            <span>x</span>
+            <span className="highlight"> ₹4999</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
