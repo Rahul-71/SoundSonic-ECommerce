6b7a6dda971b49a7722107dad188b2e8d647792f
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-08-03 21:00:04 +0530

    navigation to category page onClick of a category tile

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import "./Category.scss";
 
 const Category = ({ categories }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="shop-by-category">
       <div className="categories">
         {categories.data?.map((category) => (
-          <div key={category.id} className="category">
+          <div
+            key={category.id}
+            className="category"
+            onClick={() => navigate(`/category${category.id}`)}
+          >
             <img
               src={
                 process.env.REACT_APP_DEV_APP_KEY +
