a34b8f954739ce1eb2dae99de4e0a7b507561181
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-08-05 00:18:10 +0530

    rending product details on singleProduct page

--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -6,23 +6,38 @@ import {
   FaPinterest,
   FaTwitter,
 } from "react-icons/fa";
-import prodImg from "../../assets/products/earbuds-prod-1.webp";
+// import prodImg from "../../assets/products/earbuds-prod-1.webp";
+import useFetch from "../../hooks/useFetch";
 import RelatedProducts from "./RelatedProducts/RelatedProducts";
 
+import { useParams } from "react-router-dom";
 import "./SingleProduct.scss";
 
 const SingleProduct = () => {
+  const { id } = useParams();
+
+  // fetch the product based on prodId
+  let prodData = useFetch(`/api/products/${id}?populate=*`);
+
+  if (!prodData) return; // won't proceed further until recieve the product response
+
+  prodData = prodData.data.attributes;
+
+  let prodImg = prodData.img.data[0].attributes;
+
+  let catgData = prodData.categories.data[0].attributes;
+
   return (
     <div className="single-product-main-content">
       <div className="layout">
         <div className="single-product-page">
           <div className="left">
-            <img src={prodImg} alt="" />
+            <img src={process.env.REACT_APP_DEV_APP_KEY + prodImg.url} alt="" />
           </div>
           <div className="right">
-            <span className="name">Product Name</span>
-            <span className="price">Product price</span>
-            <span className="desc">Product description</span>
+            <span className="name">{prodData.title}</span>
+            <span className="price">{"₹" + prodData.price}</span>
+            <span className="desc">{prodData.desc}</span>
 
             <div className="cart-buttons">
               <div className="quantity-buttons">
@@ -41,7 +56,7 @@ const SingleProduct = () => {
             <div className="info-item">
               <span className="text-bold">
                 Category:
-                <span> Headphones</span>
+                <span> {catgData.title}</span>
               </span>
               <span className="text-bold">
                 Share:
